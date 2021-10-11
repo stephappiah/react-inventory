feb48e24aba4c7b8c35249b09be311d2a8893b75
Stephen Appiah <appstephen8@gmail.com>
2021-10-11 09:39:31 +0000

    initial commit #002

---
+++ components/sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class sidebar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default sidebar
