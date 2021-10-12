4fb0cfafd2dd6e0936a06777eb3dcbc91e1eb5c8
Stephen Appiah <appstephen8@gmail.com>
2021-10-12 23:55:36 +0000

    action bar & main body with table (component)

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import styles from "../styles/Home.module.css";
 import Sidebar from "../components/sidebar";
 import Main from "../components/Mainbar";
 import Navbar from "../components/navbar";
+import Mainbar from "../components/Mainbar";
+import ActionBar from "../components/ActionBar";
 // import "styles/index.css";
 
 export default function Home() {
@@ -30,12 +32,16 @@ export default function Home() {
         {/* main body with side bar */}
         <div className="row justify-content-center flex-grow-1">
           {/* side bar */}
-          <div className="col-1" style={sidebarStyle}>
+          <div className="col-2 p-0" style={sidebarStyle}>
             <Sidebar />
           </div>
 
-          <div className="col-11" style={mainbarStyle}>
-            <Main />
+          <div className="col-10" style={mainbarStyle}>
+            {/* Mainbar */}
+            <div className='row d-flex flex-column h-100'>
+              <ActionBar />
+              <Mainbar />
+            </div>
           </div>
         </div>
       </div>
